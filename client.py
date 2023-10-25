301bfc6c05aa19f48cd9e2526e0111117bfb82cf
kamari00 <mkalinichenko.007@gmail.com>
2023-10-25 11:07:08 +0200

    hw5

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 12345))
+
+    for i in range(100):
+        message = f"Message {i}"
+        client_socket.send(message.encode())  # надсилання повідомлення на сервер
+        print("Sent:", message)
+
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    start_client()
---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 12345))
+    server_socket.listen(5)
+
+    print("Server listening on port 12345...")
+
+    client_socket, addr = server_socket.accept()
+    print("Connection from", addr)
+
+    for _ in range(100):
+        data = client_socket.recv(1024)  # отримати дані розміром 1024 байти
+        print("Received:", data.decode())
+
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    start_server()
